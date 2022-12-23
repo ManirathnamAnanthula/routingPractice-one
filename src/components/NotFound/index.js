a40d65b64d3e4578859973d5fa47e322f7782099
Manirathnam <am.rathanam2011@gmail.com>
2022-12-23 06:31:10 +0000

    routingPracticeOne

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const NotFound = () => (
+  <div className="not-found-container">
+    <h1 className="notfnd-head">Not Found</h1>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+      alt="not-found"
+      className="not-found-img"
+    />
+    I love to create! I am a front-end web developer
+  </div>
+)
+
+export default NotFound
